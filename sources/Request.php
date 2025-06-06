af12b62299aba2df019909f6a80fc387c966554a
MenyongMenying <menyongmenying.main@gmail.com>
2025-06-06 16:52:29 +0700

    Membuat class utama "Request".

---
+++ sources/Request.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MenyongMenying\PHP\MenyongMenyingLibrary\Request;
+
+
+/**
+ * @author MenyongMenying <menyongmenying.main@email.com>
+ * 
+ * @version 0.0.1
+ * 
+ * @since 2025-06-06
+ * 
+ * 
+ */
+final class Request
+{
+}
