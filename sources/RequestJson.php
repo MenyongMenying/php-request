614bc99735a38aec784c683ecf56e0cfc6917422
MenyongMenying <menyongmenying.main@gmail.com>
2025-06-06 17:09:59 +0700

    Membuat class "RequestJson".

---
+++ sources/RequestJson.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MenyongMenying\PHP\MenyongMenyingLibrary\Request;
+
+
+/**
+ * @author MenyongMenying <menyongmenying.main@email.com>
+ * 
+ * @version 0.0.1
+ * 
+ * @since 2025-06-06
+ * 
+ * 
+ * 
+ */
+final class RequestJson
+{
+}
